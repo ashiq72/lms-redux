ceeda81c808d94c8299f289e5fff742c922498cc
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-08-16 15:09:50 +0600

    Router define

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { Layout, Menu, type MenuProps } from "antd";
+import { Outlet } from "react-router-dom";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -67,7 +68,7 @@ export default function MainLayout() {
               minHeight: 360,
             }}
           >
-            content
+            <Outlet />
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
---
+++ src/routes/routers.tsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+  },
+]);
+
+export default router;
