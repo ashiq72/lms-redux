b5ca1b9f9bd972ddae716d60602aa9e897077cd6
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-08-18 08:24:23 +0600

    redux setupt done

--- src/routes/routers.tsx
+++ src/routes/routers.tsx
@@ -1,50 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
 import Login from "../pages/Login";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateStudent from "../pages/admin/CreateStudent";
-import CreateAdmin from "../pages/admin/CreateAdmin";
-import CreateFaculty from "../pages/admin/CreateFaculty";
+import { adminPaths } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-    ],
   },
-
   {
     path: "/admin",
     element: <App />,
-    children: [
-      {
-        path: "dashboard",
-        element: <AdminDashboard />,
-      },
-      {
-        path: "create-admin",
-        element: <CreateAdmin />,
-      },
-      {
-        path: "create-faculty",
-        element: <CreateFaculty />,
-      },
-      {
-        path: "create-student",
-        element: <CreateStudent />,
-      },
-    ],
+    children: adminPaths,
+  },
+  {
+    path: "/faculty",
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: "/student",
+    element: <App />,
+    children: adminPaths,
   },
   {
     path: "/login",
