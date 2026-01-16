7948e7d75d362f482fa45f32ae442b21773afdda
Md ashiquzzaman <ahmedashik18k@gmail.com>
2026-01-16 14:16:25 +0600

    Faculty and department api setup done

--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -1,4 +1,8 @@
-import type { TAcademicSemester } from "../../../types/academicManagement.type";
+import type {
+  TAcademicDepartment,
+  TAcademicFaculty,
+  TAcademicSemester,
+} from "../../../types/academicManagement.type";
 import type { TQueryParam, TResponseRedux } from "../../../types/global";
 import { baseApi } from "../../api/baseApi";
 
@@ -32,10 +36,38 @@ const academicManagemantApi = baseApi.injectEndpoints({
         body: data,
       }),
     }),
-
+    getAcademicFaculties: builder.mutation({
+      query: () => {
+        return { url: "/academic-faculties", method: "GET" };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicFaculty[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
     createAcademicFaculty: builder.mutation({
       query: (data) => ({
         url: "/academic-faculties/create-academic-faculty",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    getAcademicDepartments: builder.mutation({
+      query: () => {
+        return { url: "/academic-departments", method: "GET" };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicDepartment[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    createAcademicDepartment: builder.mutation({
+      query: (data) => ({
+        url: "/academic-departments/create-academic-department",
         method: "POST",
         body: data,
       }),
@@ -46,4 +78,8 @@ const academicManagemantApi = baseApi.injectEndpoints({
 export const {
   useGetAllAcademicSemestersQuery,
   useCreateAcademicSemesterMutation,
+  useGetAcademicFacultiesMutation,
+  useCreateAcademicFacultyMutation,
+  useGetAcademicDepartmentsMutation,
+  useCreateAcademicDepartmentMutation,
 } = academicManagemantApi;
